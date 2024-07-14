9e583ba7f73200a5e0bc9a82473e8d0b25392bc2
Hyperdozy <hyperdozy@gmail.com>
2024-07-15 02:51:07 +0800

    serious mode

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
+Console.WriteLine("enough jokes, this time I'll learn for real, I will devote 2 hours daily onto learning and eventually making 1 app that I can share with my friends soon. (maybe one on a phone idk)");
