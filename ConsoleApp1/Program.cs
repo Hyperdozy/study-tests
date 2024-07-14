a987b846476e1d5d6d67e398aba32e748b28bcf0
Hyperdozy <114710263+Hyperdozy@users.noreply.github.com>
2024-07-15 02:38:45 +0800

    stfu
    too many blm rallies

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
-Console.WriteLine("Hello, my friends (no more racism)"); Console.WriteLine("Hello, my friends (no more racism)");
+Console.WriteLine("Hello, my friends (no more racism)");
